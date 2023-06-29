69c59a31f3c65e7b5f620c30b166d8f8bc55db7f
Timothy Whittaker <timothy.whittaker@datarobot.com>
2023-06-29 13:19:18 -0400

    Update geo.py

--- launch-example/geo.py
+++ launch-example/geo.py
@@ -1,7 +1,7 @@
 import wandb
 wandb.init(config = dict(a = 0.5, N = 10))
 a = wandb.config.a
-for i in range(10):
+for i in range(wandb.config.N):
   S_N = (1 - a**i) / (1 - a)
   wandb.log(dict( S_N = S_N) )
   if np.abs(a) < 1:
